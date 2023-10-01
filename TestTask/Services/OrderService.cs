83331f5a5a706b7ac9105e705ba10dce897bdd0b
pahdik <vlad0072338@mail.ru>
2023-10-01 14:43:26 +0300

    first commit

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<Order> GetOrder()
+        {
+            int MaxPrice = _context.Orders.Max(x => x.Price * x.Quantity);
+            return  _context.Orders.First(x => x.Price * x.Quantity == MaxPrice);
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return _context.Orders.Where(x => x.Quantity > 10).ToList();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Data;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Enums;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetUser()
+        {
+            var users= _context.Users.
+                GroupJoin(_context.Orders,
+                    user => user,
+                    order => order.User,
+                    (user, orders) =>
+                        new { Id = user.Id, OrdersNum = orders.Count() })
+                .ToList();
+
+            var MaxCount = users.Max(x => x.OrdersNum);
+            var Id = users.First(x => x.OrdersNum == MaxCount).Id;
+
+            return await _context.Users.FirstOrDefaultAsync(user => user.Id == Id);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users
+                .Where(user => user.Status == UserStatus.Inactive)
+                .ToListAsync();
+        }
+    }
+}
